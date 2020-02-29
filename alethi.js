5aa4d35b34e88b78056365d3859a7cd949428097
Alex Lay <AlexChrisLay@gmail.com>
2020-02-29 23:45:04 +0000

    Add alternate characters for all english letters

--- alethi.js
+++ alethi.js
@@ -1,6 +1,46 @@
-var images = {}
+const images = {}
 
-var sounds = ["A", "B", "CH", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "SH", "T", "TH", "U", "V", "Y", "Z"]
+const sounds = ["A", "B", "CH", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "SH", "T", "TH", "U", "V", "Y", "Z"]
+
+const stringSounds = new Map([
+    ["A", "A"],
+    ["B", "B"],
+    ["C", "K"],
+    ["D", "D"],
+    ["E", "E"],
+    ["F", "F"],
+    ["G", "G"],
+    ["H", "H"],
+    ["I", "I"],
+    ["J", "J"],
+    ["K", "K"],
+    ["L", "L"],
+    ["M", "M"],
+    ["N", "N"],
+    ["O", "O"],
+    ["P", "P"],
+    ["Q", "K"],
+    ["R", "R"],
+    ["S", "S"],
+    ["T", "T"],
+    ["U", "U"],
+    ["V", "V"],
+    ["W", "U"],
+    ["X", "K"], //KS substitution?
+    ["Y", "Y"],
+    ["Z", "Z"],
+    ["CH", "CH"],
+    ["SH", "SH"],
+    ["TH", "TH"],
+])
+
+function getFirstSound(text) {
+    let sound = text.substr(0, 2)
+    if (!stringSounds.has(sound)) {
+        sound = text.substr(0, 1)
+    }
+    return stringSounds.get(sound)
+}
 
 function loadImages() {
     return Promise.all(
@@ -32,15 +72,12 @@ function generateText(){
         let lineSounds = 0
         let remainder = line
         while (remainder.length > 0) {
-            let sound = remainder.substr(0, 2)
-            if (!sounds.includes(sound)) {
-                sound = remainder.substr(0, 1)
-            }
+            let sound = getFirstSound(remainder)
             if (sounds.includes(sound)) {
                 const xOffSet = (151 * lineSounds) - 174.5
                 const yOffSet = imgHeight-651.86218
 
-                let glyph = images[sound].cloneNode(true)
+                const glyph = images[sound].cloneNode(true)
                 glyph.setAttribute("transform", "translate("+xOffSet+","+yOffSet+")")
                 svg.appendChild(glyph)
             } else {
