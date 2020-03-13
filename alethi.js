eddbfb1f233019d3ac9b53a4e0edd852405a4b0e
Alex Lay <AlexChrisLay@gmail.com>
2020-03-13 00:06:52 +0000

    Move js functions into Alethi namespace

--- alethi.js
+++ alethi.js
@@ -3,51 +3,51 @@ const Alethi = {
     lineHeight : 61,
     lineSpacing : 10,
     wordSpacing : 10,
+
+    images : new Map(),
+    imageWidths : new Map(),
+
+    sounds : ["A", "B", "CH", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "SH", "T", "TH", "U", "V", "Y", "Z","]["],
+
+    stringSounds : new Map([
+        ["A", "A"],
+        ["B", "B"],
+        ["C", "K"],
+        ["D", "D"],
+        ["E", "E"],
+        ["F", "F"],
+        ["G", "G"],
+        ["H", "H"],
+        ["I", "I"],
+        ["J", "J"],
+        ["K", "K"],
+        ["L", "L"],
+        ["M", "M"],
+        ["N", "N"],
+        ["O", "O"],
+        ["P", "P"],
+        ["Q", "K"],
+        ["R", "R"],
+        ["S", "S"],
+        ["T", "T"],
+        ["U", "U"],
+        ["V", "V"],
+        ["W", "U"],
+        ["Y", "Y"],
+        ["Z", "Z"],
+        ["CH", "CH"],
+        ["SH", "SH"],
+        ["TH", "TH"],
+        ["][", "]["], // max line height indicator
+        [" ", " "],
+    ]),
+
+    imageUrl : null,
 }
 
-const images = new Map()
-const imageWidths = new Map()
-
-const sounds = ["A", "B", "CH", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "SH", "T", "TH", "U", "V", "Y", "Z","]["]
-
-const stringSounds = new Map([
-    ["A", "A"],
-    ["B", "B"],
-    ["C", "K"],
-    ["D", "D"],
-    ["E", "E"],
-    ["F", "F"],
-    ["G", "G"],
-    ["H", "H"],
-    ["I", "I"],
-    ["J", "J"],
-    ["K", "K"],
-    ["L", "L"],
-    ["M", "M"],
-    ["N", "N"],
-    ["O", "O"],
-    ["P", "P"],
-    ["Q", "K"],
-    ["R", "R"],
-    ["S", "S"],
-    ["T", "T"],
-    ["U", "U"],
-    ["V", "V"],
-    ["W", "U"],
-    ["Y", "Y"],
-    ["Z", "Z"],
-    ["CH", "CH"],
-    ["SH", "SH"],
-    ["TH", "TH"],
-    ["][", "]["], // max line height indicator
-    [" ", " "],
-])
-
-let imageUrl = null
-
-function loadImages() {
+Alethi.loadImages = function loadImages() {
     return Promise.all(
-        sounds.map(sound =>
+        Alethi.sounds.map(sound =>
             fetch("symbols/" + sound + ".svg")
             .then(response => response.text())
             .then(text => text.replace(/stroke:#[0-9a-fA-F]*;?/g, ''))
@@ -55,14 +55,14 @@ function loadImages() {
             .then(text => (new window.DOMParser()).parseFromString(text, "text/xml"))
             .then(svgNode => {
                 console.log(sound, svgNode)
-                images.set(sound, svgNode.querySelector("g"))
-                imageWidths.set(sound, parseInt(svgNode.documentElement.getAttribute("width")))
+                Alethi.images.set(sound, svgNode.querySelector("g"))
+                Alethi.imageWidths.set(sound, parseInt(svgNode.documentElement.getAttribute("width")))
             })
         )
     )
 }
 
-function getSymbols() {
+Alethi.getSymbols = function getSymbols() {
     let text = document.getElementById("sourceText").value.trim().toUpperCase()
     const autoHeightMarkers = document.getElementById("autoHeightMarkCheckbox").checked
     text = text.trim()
@@ -84,11 +84,11 @@ function getSymbols() {
         let index = 0
         while (index < line.length) {
             let sound = line.substr(index, 2)
-            if (!stringSounds.has(sound)) {
+            if (!Alethi.stringSounds.has(sound)) {
                 sound = line.substr(index, 1)
             }
-            if (stringSounds.has(sound)) {
-                symbols.push(stringSounds.get(sound))
+            if (Alethi.stringSounds.has(sound)) {
+                symbols.push(Alethi.stringSounds.get(sound))
             }
             index += sound.length
         }
@@ -97,22 +97,22 @@ function getSymbols() {
     return lines
 }
 
-function generateText(){
+Alethi.generateText = function generateText() {
     let imgWidth = 0
     let imgHeight = 0
     const svg = document.getElementById("drawingArea")
     while (svg.hasChildNodes()) {
         svg.removeChild(svg.firstChild)
     }
-    setBackground()
-    setForeground()
+    Alethi.setBackground()
+    Alethi.setForeground()
 
     const italicAngle = document.getElementById("italicsAngleInput").value
     const italicOffset = Alethi.lineHeight * Math.tan(italicAngle*Math.PI/180)
 
     const lineGroups = []
     const lineWidths = []
-    const lines = getSymbols()
+    const lines = Alethi.getSymbols()
     for (let i = 0; i < lines.length; i++) {
         if (i != 0) {
             imgHeight += Alethi.lineSpacing
@@ -124,10 +124,10 @@ function generateText(){
         const lineGroup = document.createElementNS("http://www.w3.org/2000/svg", "g")
         for (const symbol of lines[i]) {
             if (symbol != " ") {
-                const glyph = images.get(symbol).cloneNode(true)
+                const glyph = Alethi.images.get(symbol).cloneNode(true)
                 glyph.setAttribute("transform", "translate("+lineWidth+",0)")
                 lineGroup.appendChild(glyph)
-                lineWidth += imageWidths.get(symbol)
+                lineWidth += Alethi.imageWidths.get(symbol)
             } else {
                 lineWidth += Alethi.wordSpacing
             }
@@ -164,10 +164,10 @@ function generateText(){
     const description = document.createElementNS("http://www.w3.org/2000/svg", "desc")
     description.textContent = "The text '" + document.getElementById("sourceText").value + "' displayed in womens script from The Stormlight Archive"
     svg.appendChild(description)
-    displayImage()
+    Alethi.displayImage()
 }
 
-function displayImage() {
+Alethi.displayImage = function displayImage() {
     const format = document.querySelector("input[name=format]:checked").value
 
     const svg = document.getElementById("drawingArea")
@@ -176,15 +176,15 @@ function displayImage() {
 
     const domURL = window.URL || window.webkitURL || window
     const svgBlob = new Blob([data], {type: "image/svg+xml;charset=utf-8"})
-    if (imageUrl != null) {
-        domURL.revokeObjectURL(imageUrl)
+    if (Alethi.imageUrl != null) {
+        domURL.revokeObjectURL(Alethi.imageUrl)
     }
-    imageUrl = domURL.createObjectURL(svgBlob)
+    Alethi.imageUrl = domURL.createObjectURL(svgBlob)
     const imageTag = document.getElementById("outputImage")
 
     if (format == "svg") {
         // Load the svg blob into the DOM
-        imageTag.src = imageUrl
+        imageTag.src = Alethi.imageUrl
     } else if (format == "png") {
         // Render the svg blob to a png blob, and load that into the DOM
         const canvas = document.createElement("canvas")
@@ -195,17 +195,17 @@ function displayImage() {
         const image = new Image()
         image.onload = function(){
             context.drawImage(image, 0, 0, svg.getAttribute("width"), svg.getAttribute("height"))
-            domURL.revokeObjectURL(imageUrl)
+            domURL.revokeObjectURL(Alethi.imageUrl)
             canvas.toBlob((blob) => {
-                imageUrl = domURL.createObjectURL(blob)
-                imageTag.src = imageUrl
+                Alethi.imageUrl = domURL.createObjectURL(blob)
+                imageTag.src = Alethi.imageUrl
             })
         }
-        image.src = imageUrl
+        image.src = Alethi.imageUrl
     }
 }
 
-function setBackground() {
+Alethi.setBackground = function setBackground() {
     const transparent = document.getElementById("transparentBgCheckbox").checked
     if (!transparent) {
         const colour = document.getElementById("bgColourPicker").value
@@ -218,17 +218,17 @@ function setBackground() {
     }
 }
 
-function setForeground() {
+Alethi.setForeground = function setForeground() {
     const colour = document.getElementById("fgColourPicker").value
     const scale = document.getElementById("scaleInput").value
     const width = document.getElementById("strokeWidthInput").value / scale
     const style = "stroke:"+colour+";stroke-width:"+width+"px;"
-    for (const symbol of images.values()) {
+    for (const symbol of Alethi.images.values()) {
         symbol.setAttribute("style", style)
     }
 }
 
-window.onload = function(){
+window.onload = function() {
     document.getElementById("sourceText").value =
         "Szeth son son Vallano\n" +
         "Truthless of Shinovar\n" +
@@ -236,9 +236,9 @@ window.onload = function(){
         "he was to kill a king"
 
     document.querySelectorAll("input, textarea").forEach((input) => {
-        input.onchange = generateText
+        input.onchange = Alethi.generateText
     })
 
-    loadImages()
-    .then(generateText)
+    Alethi.loadImages()
+    .then(Alethi.generateText)
 }
